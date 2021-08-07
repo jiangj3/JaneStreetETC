78bf5aaa4ee540b1ea7a15495ac52d29f7026e68
alanji2024 <ajji@princeton.edu>
2021-08-07 16:10:59 -0400

    asdf

--- adr.py
+++ adr.py
@@ -1,5 +1,10 @@
 def act(id, name, bs, price, size):
     return "ADD " + id + " "+ name + " " + bs + " " + price+ " "+size
 
+def convert(id, name, bs, size):
+    return "CONVERT " + id + " "+name+ " "+bs + " "+size
+
 if valebuy < (valbzsell-buffer):
-    buy()
+    act(id, "VALE", "BUY", valebuy, 2)
+    convert(id, "VALE", "SELL", 2)
+    act(id, "VALBZ", "SELL", valbzsell, 2)
