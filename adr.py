2c3671f5e1159208c1dc11ff4fa33e3938a37d75
alanji2024 <ajji@princeton.edu>
2021-08-07 16:16:50 -0400

    asdfasdf

--- adr.py
+++ adr.py
@@ -1,10 +1,15 @@
 def act(id, name, bs, price, size):
-    return "ADD " + id + " "+ name + " " + bs + " " + price+ " "+size
+    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": name, "dir": bs, "price": price, "size": size})
 
 def convert(id, name, bs, size):
-    return "CONVERT " + id + " "+name+ " "+bs + " "+size
+    write_to_exchange(exchange, {"type": "convert", "order_id": id, "symbol": name, "dir": bs, "size": size})
 
 if valebuy < (valbzsell-buffer):
     act(id, "VALE", "BUY", valebuy, 2)
     convert(id, "VALE", "SELL", 2)
     act(id, "VALBZ", "SELL", valbzsell, 2)
+
+if valbzbuy < (valesell-buffer):
+    act(id, "VALBZ", "BUY", valbzbuy, 2)
+    convert(id, "VALE", "BUY", 2)
+    act(id, "VALE", "BUY", valesell, 2)
